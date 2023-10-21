4a6733046e20f49bbc45d9b052d2db2e8291f249
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-10-21 23:54:02 +0600

    small bug fix

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = () => {
   return (
     <div>
       <div
-        className="hero min-h-screen"
+        className="hero lg:min-h-screen"
         style={{
           backgroundImage:
             "url(https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1770)",
